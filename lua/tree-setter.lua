ba9fcb2a4168d8ff1ccc5641b41463eabde97eee
TornaxO7 <tornax07@gmail.com>
2021-06-17 18:10:56 +0200

    Documentation
    Added some docs to the main-entry point of tree-setter

--- lua/tree-setter.lua
+++ lua/tree-setter.lua
@@ -5,7 +5,13 @@ local M = {}
 function M.init()
     require("nvim-treesitter").define_modules({
         tree_setter = {
+            -- the file with the "main-code" of the module
             module_path = "tree-setter.main",
+
+            -- Look if the module supports the current language by looking up,
+            -- if there's an appropriate query file to it. For example if we're
+            -- currently editing a C file, then this function looks if there's a
+            -- "tree-setter/queries/c/tsetter.scm" file.
             is_supported = function(lang)
                 return queries.get_query(lang, 'tsetter') ~= nil
             end
