9f13404c1298d31408c270e2a2ca50cdd9c990db
Filip <filipnajjar@yahoo.com>
2023-08-03 09:15:28 -0400

    feat(java): add java queries

--- queries/java/tsetter.scm
+++ queries/java/tsetter.scm
@@ -1,5 +1,50 @@
-; Insert semicolon at the end of a statement
-((statement_termination) @semicolon)
+;; local variables
+;;      String s = "";
+(local_variable_declaration
+    type: (_)
+    declarator: (_) @semicolon
+)
 
-; Position to insert semicolon
-((source_file) @insertion-point)
+;; fields
+;;      private Map<String, String> data;
+(field_declaration
+    type: (_)
+    declarator: (_) @semicolon
+)
+
+;; assignments
+;;      this.data = new HashMap<>();
+((expression_statement
+    (assignment_expression
+        left: (_)
+        right: (_)
+    )
+) @semicolon)
+
+;; methods
+;;      this.client.configure(config);
+((expression_statement
+    (method_invocation
+        object: (_)
+        name: (_)
+        arguments: (_)
+    )
+) @semicolon)
+
+;; return statements
+;;      return 0;
+((return_statement
+    (_)
+) @semicolon)
+
+;; package declaration
+;;      package example;
+((package_declaration
+    (_)
+) @semicolon)
+
+;; import
+;;      import java.util.Map;
+((import_declaration
+    (_)
+) @semicolon)
