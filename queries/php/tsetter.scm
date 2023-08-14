f145e61c6492afd946fdd6da1c51dff65275bf87
laureanray <laureanraybahala@gmail.com>
2023-08-15 01:59:35 +0800

    feat(php): add support for php variable and arrays

---
+++ queries/php/tsetter.scm
@@ -0,0 +1,20 @@
+;; --------------
+;; Variables
+;; --------------
+;; PHP variable declaration
+;; Example:
+;;      $foo = 1
+;;      $bar = "hello"
+(expression_statement
+  (variable_name) @equals
+  )
+
+;; --------------
+;; Associative Arrays
+;; --------------
+;; PHP Associative arrays
+;; Example:
+;;      $foo['bar'] = 0
+(expression_statement
+  (subscript_expression) @equals
+  )
